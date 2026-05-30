909642a4a77b4bab4b34b66f4336d78313e47d03
lamdx4 <dagxuanlam@gmail.com>
2026-05-31 00:08:10 +0700

    fix: add FromHeader binding to RemoveAvailability RequesterId to ensure owner checking works flawlessly

--- src/Airbnb.PropertyService/Features/ManageAvailability/RemoveAvailability/Models.cs
+++ src/Airbnb.PropertyService/Features/ManageAvailability/RemoveAvailability/Models.cs
@@ -1,7 +1,12 @@
+using FastEndpoints;
 using Mediator;
 using Airbnb.ServiceDefaults.Infrastructure;
 
 namespace Airbnb.PropertyService.Features.ManageAvailability.RemoveAvailability;
 
-public record Request(Guid PropertyId, Guid AvailabilityId, Guid RequesterId) 
-    : Mediator.ICommand<ApiResponse<bool>>;
+public record Request(Guid PropertyId, Guid AvailabilityId) 
+    : Mediator.ICommand<ApiResponse<bool>>
+{
+    [FromHeader("X-User-Id")]
+    public Guid RequesterId { get; init; }
+}
